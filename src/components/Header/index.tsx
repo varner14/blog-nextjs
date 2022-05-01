a6b04114dd50f71c486872be4172dff40c7b9ca0
VarnerCardoso <varner.silveira@gmail.com>
2022-05-01 18:23:39 -0300

    first commit

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,17 @@
+import { SignInButton } from '../SignInButton';
+import styles from './styles.module.scss';
+
+export function Header() {
+  return (
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
+       <h2>React World</h2>
+        <nav>
+          <a className={styles.active}>Home</a>
+          <a >Posts</a>
+        </nav>
+        <SignInButton />
+      </div>
+    </header>
+  );
+}
